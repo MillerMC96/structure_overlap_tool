f3a35cf8a783dfa9cdb81d5920f4fd91317b60f0
MillerMC96 <pywang@ucdavis.edu>
2020-04-20 11:47:22 -0700

    initialize

---
+++ src/find_transform_mat.cpp
@@ -0,0 +1,26 @@
+#include <glm/glm.hpp>
+
+#define GLM_FORCE_CTOR_INIT //initialize matrix to 0
+
+typedef glm::mat4 transform_mat
+
+class atom
+{
+    public:
+        atom(float x_coordinate, float y_coordinate, float z_coordinate)
+        {
+            x_coor = x_coordinate;
+            y_coor = y_coordinate;
+            z_coor = z_coordinate;
+        }
+        atom(void)
+        {
+            x_coor = 0.0;
+            y_coor = 0.0;
+            z_coor = 0.0;
+        }
+
+        float x_coor;
+        float y_coor;
+        float z_coor;
+}
