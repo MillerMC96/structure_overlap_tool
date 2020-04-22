e3719273a8dce8559a46058a2d482008f1b5d7a5
MillerMC96 <pywang@ucdavis.edu>
2020-04-22 12:30:37 -0700

    switched to vec4 representation of atom coordinates

--- include/atom.h
+++ include/atom.h
@@ -1,28 +1,27 @@
+typedef glm::vec4 vector;
+
 class atom
 {
     public:
         atom(int atom_ID, int model_num,
              float x_coordinate, float y_coordinate, float z_coordinate)
         {
-            x_coor = x_coordinate;
-            y_coor = y_coordinate;
-            z_coor = z_coordinate;
+            coords[0] = x_coordinate;
+            coords[1] = y_coordinate;
+            coords[2] = z_coordinate;
             ID = atom_ID;
             model = model_num;
         }
 
         atom(void)
         {
-            x_coor = 0.0;
-            y_coor = 0.0;
-            z_coor = 0.0;
+            coords = vector(0.0);
             ID = 0;
             model = 0;
         }
 
-        float x_coor;
-        float y_coor;
-        float z_coor;
+        //coordinate in glm::vec4 format
+        vector coords;
         int ID;
         int model;
 };
