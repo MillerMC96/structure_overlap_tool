d04dd252e38b9a82d355da32d69090cc198db31b
MillerMC96 <pywang@ucdavis.edu>
2020-04-21 23:25:26 -0700

    split into include file

--- src/find_transform_mat.cpp
+++ src/find_transform_mat.cpp
@@ -1,43 +1,4 @@
-#include <iostream>
-#include <glm/glm.hpp>
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <string>
-
-#define GLM_FORCE_CTOR_INIT //initialize matrix to 0
-
-typedef glm::mat4 transform_mat;
-typedef glm::vec4 vector;
-
-class atom
-{
-    public:
-        atom(int atom_ID, int model_num,
-             float x_coordinate, float y_coordinate, float z_coordinate)
-        {
-            x_coor = x_coordinate;
-            y_coor = y_coordinate;
-            z_coor = z_coordinate;
-            ID = atom_ID;
-            model = model_num;
-        }
-
-        atom(void)
-        {
-            x_coor = 0.0;
-            y_coor = 0.0;
-            z_coor = 0.0;
-            ID = 0;
-            model = 0;
-        }
-
-        float x_coor;
-        float y_coor;
-        float z_coor;
-        int ID;
-        int model;
-};
+#include "find_transform_mat.h"
 
 int main(int argc, char* argv[])
 {
