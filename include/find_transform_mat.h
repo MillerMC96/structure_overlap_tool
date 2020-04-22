6cc04ec8e58135d48ae1749f5cacd59b8f907a51
MillerMC96 <pywang@ucdavis.edu>
2020-04-21 23:38:10 -0700

    function interfaces for getting translation and rotation matrices

--- include/find_transform_mat.h
+++ include/find_transform_mat.h
@@ -10,3 +10,8 @@
 typedef glm::mat4 transform_mat;
 typedef glm::vec4 vector;
 
+//returns the translation matrix between two atoms
+transform_mat get_translation_matrix(vector& atom1, vector& atom2);
+
+//returns the rotation matrix between two atoms about a given pivot
+transform_mat get_rot_mat_about_an_axis(vector& atom1, vector& atom2, vector& pivot_atom);
