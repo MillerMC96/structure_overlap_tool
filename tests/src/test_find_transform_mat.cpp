65256d9ca69fedb2381085462aee217bf610b453
MillerMC96 <pywang@ucdavis.edu>
2020-04-24 10:54:07 -0700

    test_translation done

--- tests/src/test_find_transform_mat.cpp
+++ tests/src/test_find_transform_mat.cpp
@@ -6,15 +6,25 @@
 //testing get_translation_matrix
 int test_get_translation_matrix(void)
 {
-    vector atom1 = vector(2.0, 2.0, 2.0, 1.0);
-    vector atom2 = vector(1.0, 1.0, 1.0, 1.0);
+    vector atom1 = vector(1.0, 1.0, 1.0, 1.0);
+    vector atom2 = vector(2.0, 2.0, 2.0, 1.0);
+    vector atom3 = vector(2.0, 2.0, 2.0, 1.0);
+    vector atom4 = vector(1.0, 1.0, 1.0, 1.0);
 
     transform_mat result = get_translation_matrix(atom1, atom2);
-
+    transform_mat result_neg = get_translation_matrix(atom3, atom4);
+    
+    //positive translation
     assert(result[3][0] == 1.0);
     assert(result[3][1] == 1.0);
     assert(result[3][2] == 1.0);
     assert(result[3][3] == 1.0);
+
+    //negative translation
+    assert(result_neg[3][0] == -1.0);
+    assert(result_neg[3][1] == -1.0);
+    assert(result_neg[3][2] == -1.0);
+    assert(result_neg[3][3] == 1.0);
 
     return 0;
 }
