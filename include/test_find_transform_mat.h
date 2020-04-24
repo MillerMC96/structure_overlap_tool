3c0a29bab816fc33d583b46251d509b98d5b6a55
MillerMC96 <pywang@ucdavis.edu>
2020-04-23 22:37:13 -0700

    better interface and finished test_get_translation_matrix

--- include/test_find_transform_mat.h
+++ include/test_find_transform_mat.h
@@ -2,10 +2,10 @@
 #include <cassert>
 
 //testing get_translation_matrix
-int test_get_translation_matrix(vector& atom1, vector& atom2);
+int test_get_translation_matrix(void);
 
 //testing get_rot_mat_about_an_atom
-int test_get_rot_mat_about_an_atom(vector& atom1, vector& atom2, vector& pivot_atom);
+int test_get_rot_mat_about_an_atom(void);
 
 //testing get_rot_mat_about_an_axis
-int test_get_rot_mat_about_an_axis(vector& atom1, vector& atom2, std::vector<vector>& axis_atoms);
+int test_get_rot_mat_about_an_axis(void);
--- tests/test_find_transform_mat.cpp
+++ tests/test_find_transform_mat.cpp
@@ -1,21 +1,30 @@
 #include "test_find_transform_mat.h"
 
 //testing get_translation_matrix
-int test_get_translation_matrix(vector& atom1, vector& atom2)
+int test_get_translation_matrix(void)
 {
+    vector atom1 = vector(2.0, 2.0, 2.0, 1.0);
+    vector atom2 = vector(1.0, 1.0, 1.0, 1.0);
+
+    transform_mat result = get_translation_matrix(atom1, atom2);
+
+    assert(result[3][0], 1.0);
+    assert(result[3][1], 1.0);
+    assert(result[3][2], 1.0);
+    assert(result[3][3], 1.0);
 
     return 0;
 }
 
 //testing get_rot_mat_about_an_atom
-int test_get_rot_mat_about_an_atom(vector& atom1, vector& atom2, vector& pivot_atom)
+int test_get_rot_mat_about_an_atom(void)
 {
 
     return 0;
 }
 
 //testing get_rot_mat_about_an_axis
-int test_get_rot_mat_about_an_axis(vector& atom1, vector& atom2, std::vector<vector>& axis_atoms)
+int test_get_rot_mat_about_an_axis(void)
 {
 
     return 0;
